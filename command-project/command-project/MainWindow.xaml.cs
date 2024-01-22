0dc1556023858b99bf0894d9a221bea39a6e3519
Mark111P <markusperevezentsev@gmail.com>
2024-01-22 15:34:59 +0100

    Исправлена версия

--- command-project/command-project/MainWindow.xaml.cs
+++ command-project/command-project/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace command_project
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml aaaaaaaa
     /// </summary>
     public partial class MainWindow : Window
     {
