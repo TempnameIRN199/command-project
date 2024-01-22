7abb7f7fad4ca4cb0937f6734c0c5df0750cc680
Mark111P <markusperevezentsev@gmail.com>
2024-01-22 16:29:36 +0100

    Добавил функцию конвертации из поля баз данных в лист навыков

--- command-project/command-project/MainWindow.xaml.cs
+++ command-project/command-project/MainWindow.xaml.cs
@@ -15,14 +15,35 @@ using System.Windows.Shapes;
 
 namespace command_project
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        //Из поля баз данных перевод в лист навыков
+        List<Skill> GetSkills(string skillsText)
+        {
+            //Из C#^5|SQL^2 в Skill(){Name = "C#", Time = 5}, Skill(){Name = "SQL", Time = 2}
+            List<Skill> skills = new List<Skill>();
+
+            List<string> skillsTextParts = skillsText.Split('|').ToList();
+            foreach (string skill in skillsTextParts)
+            {
+                List<string> parts = skillsText.Split('^').ToList();
+                skills.Add(new Skill() { Name = parts[0], Time = int.Parse(parts[1]) });
+            }
+
+            return skills;
+        }
+    }
+
+    class Skill
+    {
+        public string Name { get; set; }
+        public int Time { get; set; }
+
+        public Skill() { }
     }
 }
