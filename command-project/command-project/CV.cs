e60dc28f87e2360be69c564f372fd0ece0ddc784
merphyth <senyachumak@gmail.com>
2024-01-22 17:43:28 +0100

    классы

---
+++ command-project/command-project/CV.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace command_project
+{
+    //резюме (Curriculum vitae)
+    internal class CV
+    {
+        //айди в таблице
+        private int Id { get; set; }
+        //юзер, который сделал резюме
+        private Users user { get; set; }
+        //когда было сделано резюме
+        private DateTime CVDate { get; set; }
+        //пример C#^5|SQL^2 (сначала идет язык, знак ^ и цифра после него - сколько лет у юзера стаж работы с этим языком)
+        private string Skills { get; set; }
+        //юзер пишет инфо про себя(то что он хочет сказать работадателю)
+        private string UserInfo { get; set; }
+    }
+}
---
+++ command-project/command-project/RequestUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace command_project
+{
+    //когда юзер принимает вакансию
+    //если что,работодатель есть в вакансии(он ее создавал)
+    internal class RequestUser
+    {
+        private int Id { get; set; }
+        //работник,который принял вакансию
+        private Users Worker { get; set; }
+        //какую именно вакансию
+        private Requests Request { get; set; }
+        //когда работник принял вакансию
+        private DateTime RequestTime { get; set; }
+        //принял ли работодатель работника(3 варианта:Accepted UnderReview Denied)
+        private string Status { get; set; }
+    }
+}
