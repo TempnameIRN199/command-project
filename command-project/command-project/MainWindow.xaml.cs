33cc96bf719e67f6a71bd71b171a7cf2a1e82351
Mark111P <markusperevezentsev@gmail.com>
2024-01-22 16:54:02 +0100

    Добавил функцию проверки достаточно ли навыков

--- command-project/command-project/MainWindow.xaml.cs
+++ command-project/command-project/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace command_project
         public MainWindow()
         {
             InitializeComponent();
+
+            
         }
 
         //Из поля баз данных перевод в лист навыков
@@ -31,11 +33,39 @@ namespace command_project
             List<string> skillsTextParts = skillsText.Split('|').ToList();
             foreach (string skill in skillsTextParts)
             {
-                List<string> parts = skillsText.Split('^').ToList();
+                List<string> parts = skill.Split('^').ToList();
                 skills.Add(new Skill() { Name = parts[0], Time = int.Parse(parts[1]) });
             }
 
             return skills;
+        }
+
+
+        //Проверка достаточно ли навыков у пользователя для резюме по листу его навыков и листу необходимых навыков
+        bool IsEnoughSkills(List<Skill> userSkills, List<Skill> requiredSkills) 
+        {
+            bool isEnough = true;
+
+            foreach (Skill skill in requiredSkills) 
+            {
+                if (requiredSkills.Select(i => i.Name).Contains(skill.Name)) 
+                {
+                    Skill userCurrentSkill = userSkills.First(i => i.Name == skill.Name);
+                    Skill requiredCurrentSkill = requiredSkills.First(i => i.Name == skill.Name);
+                    if (userCurrentSkill.Time < requiredCurrentSkill.Time)
+                    {
+                        isEnough = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    isEnough = false;
+                    break;
+                }
+            }
+
+            return isEnough;
         }
     }
 
