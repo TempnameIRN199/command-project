c2153d45bfa59eccea50215488c10a2ca3f7dc02
TempnameIRN199 <vityayuzbekov602@gmail.com>
2024-01-24 17:36:22 +0200

    owtiss

--- command-project/command-project/design/admin/Menu.xaml.cs
+++ command-project/command-project/design/admin/Menu.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace command_project.design.admin
 {
