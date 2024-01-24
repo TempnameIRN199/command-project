6e8cd34c9c8c8c9a5461fe502dfc1477d588839c
merphyth <senyachumak@gmail.com>
2024-01-24 15:50:54 +0100

    Skills таблица

---
+++ command-project/command-project/Skills.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace command_project
+{
+    internal class Skills
+    {
+        private int Id { get; set; }
+        
+        private string Name { get; set; }
+    }
+}
