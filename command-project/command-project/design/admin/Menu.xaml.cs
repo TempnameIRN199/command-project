c9d0b0764c84d9c4aca484c1492acf7cbec5748e
TempnameIRN199 <vityayuzbekov602@gmail.com>
2024-01-29 17:24:41 +0200

    fix error

--- command-project/command-project/design/admin/Menu.xaml.cs
+++ command-project/command-project/design/admin/Menu.xaml.cs
@@ -13,8 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using LiveCharts;
-using LiveCharts.Wpf;
 
 namespace command_project.design.admin
 {
