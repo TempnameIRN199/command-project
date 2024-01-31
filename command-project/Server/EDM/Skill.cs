4130926039eb971eded8ca5692a77585229651d0
Mark111P <markusperevezentsev@gmail.com>
2024-01-31 15:27:55 +0100

    +пару функций

---
+++ command-project/Server/EDM/Skill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work.EDM
+{
+        public class Skill
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+}
