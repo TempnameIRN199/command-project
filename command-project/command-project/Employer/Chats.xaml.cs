b1556738f232132d134f763b2a5ede28b7cab6f2
kriomanser65 <106473729+Kriomanser65@users.noreply.github.com>
2024-02-21 18:28:38 +0200

    02.21

---
+++ command-project/command-project/Employer/Chats.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace command_project.Employer
+{
+    /// <summary>
+    /// Interaction logic for Chats.xaml
+    /// </summary>
+    public partial class Chats : Window
+    {
+        private MainWindow profileWindow;
+        private Chats chatsWindow;
+        private CreateVacancy createVacancyWindow;
+        private MyVacancy myVacancyWindow;
+        public Chats()
+        {
+            InitializeComponent();
+            // Видаліть рядок, який створює новий екземпляр Chats
+            // chatsWindow = new Chats(); 
+            createVacancyWindow = new CreateVacancy();
+            myVacancyWindow = new MyVacancy();
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void MainTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (MainTabControl.SelectedItem == ProfileTabItem)
+            {
+                if (!this.IsVisible)
+                    this.Show();
+            }
+            else if (MainTabControl.SelectedItem == ChatsTabItem)
+            {
+                if (!chatsWindow.IsVisible)
+                    chatsWindow.Show();
+            }
+            else if (MainTabControl.SelectedItem == CreateVacancyTabItem)
+            {
+                if (!createVacancyWindow.IsVisible)
+                    createVacancyWindow.Show();
+            }
+            else if (MainTabControl.SelectedItem == MyVacancyTabItem)
+            {
+                if (!myVacancyWindow.IsVisible)
+                    myVacancyWindow.Show();
+            }
+        }
+    }
+}
---
+++ command-project/command-project/Employer/Profile.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace command_project.Employer
+{
+    /// <summary>
+    /// Interaction logic for Profile.xaml
+    /// </summary>
+    public partial class Profile : Window
+    {
+        private Profile profileWindow;
+        private Chats chatsWindow;
+        private CreateVacancy createVacancyWindow;
+        private MyVacancy myVacancyWindow;
+        public Profile()
+        {
+            InitializeComponent();
+            //profileWindow = new MainWindow(); 
+            chatsWindow = new Chats();
+            createVacancyWindow = new CreateVacancy();
+            myVacancyWindow = new MyVacancy();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void MainTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (MainTabControl.SelectedItem == ProfileTabItem)
+            {
+                if (!this.IsVisible)
+                    this.Show();
+            }
+            else if (MainTabControl.SelectedItem == ChatsTabItem)
+            {
+                if (!chatsWindow.IsVisible)
+                    chatsWindow.Show();
+            }
+            else if (MainTabControl.SelectedItem == CreateVacancyTabItem)
+            {
+                if (!createVacancyWindow.IsVisible)
+                    createVacancyWindow.Show();
+            }
+            else if (MainTabControl.SelectedItem == MyVacancyTabItem)
+            {
+                if (!myVacancyWindow.IsVisible)
+                    myVacancyWindow.Show();
+            }
+        }
+    }
+}
