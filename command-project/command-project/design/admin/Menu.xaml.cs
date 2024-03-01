30ef41c5157418cf9bcdfef0b2ca6ec5fa5d1b8b
TempnameIRN199 <vityayuzbekov602@gmail.com>
2024-03-01 16:55:56 +0200

    .

--- command-project/command-project/design/admin/Menu.xaml.cs
+++ command-project/command-project/design/admin/Menu.xaml.cs
@@ -70,43 +70,43 @@ namespace command_project.design.admin
 
 
 
-            SeriesCollection = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "Registered Users",
-                    Values = new ChartValues<int>(),
-                    LineSmoothness = 0.8,
-                    PointGeometry = new EllipseGeometry(new Point(0, 0), 10, 10),
-                    Stroke = new SolidColorBrush(Color.FromRgb(255, 0, 255)),
-                    StrokeThickness = 4
-                }
-            };
-
-            for (int i = 0; i < numberOfMonths; i++)
-            {
-                DateTime currentDate = DateTime.Now.AddMonths(i);
-                list.Add(currentDate.ToString("yyyy-MM"));
-
-                int userCount = GetUserDataForMonth(currentDate);
-                SeriesCollection[0].Values.Add(userCount);
-            }
-            statsDash.AxisX.Add(new Axis
-            {
-                Labels = list.ToArray(),
-                Title = "Дати",
-            });
-            statsDash.AxisY.Add(new Axis
-            {
-                Title = "Кількість резюме",
-                MinValue = -500,
-                MaxValue = 500
-            });
-
-            DataContext = this;
-
-            statsDash.AxisX[0].LabelFormatter = value => new DateTime((long)(value * TimeSpan.FromDays(30).Ticks)).ToString("yyyy-MM");
-            statsDash.AxisY[0].LabelFormatter = value => value.ToString("N");
+            //SeriesCollection = new SeriesCollection
+            //{
+            //    new LineSeries
+            //    {
+            //        Title = "Registered Users",
+            //        Values = new ChartValues<int>(),
+            //        LineSmoothness = 0.8,
+            //        PointGeometry = new EllipseGeometry(new Point(0, 0), 10, 10),
+            //        Stroke = new SolidColorBrush(Color.FromRgb(255, 0, 255)),
+            //        StrokeThickness = 4
+            //    }
+            //};
+            //
+            //for (int i = 0; i < numberOfMonths; i++)
+            //{
+            //    DateTime currentDate = DateTime.Now.AddMonths(i);
+            //    list.Add(currentDate.ToString("yyyy-MM"));
+            //
+            //    int userCount = GetUserDataForMonth(currentDate);
+            //    SeriesCollection[0].Values.Add(userCount);
+            //}
+            //statsDash.AxisX.Add(new Axis
+            //{
+            //    Labels = list.ToArray(),
+            //    Title = "Дати",
+            //});
+            //statsDash.AxisY.Add(new Axis
+            //{
+            //    Title = "Кількість резюме",
+            //    MinValue = -500,
+            //    MaxValue = 500
+            //});
+            //
+            //DataContext = this;
+            //
+            //statsDash.AxisX[0].LabelFormatter = value => new DateTime((long)(value * TimeSpan.FromDays(30).Ticks)).ToString("yyyy-MM");
+            //statsDash.AxisY[0].LabelFormatter = value => value.ToString("N");
         }
 
         private int GetUserDataForMonth(DateTime month)
